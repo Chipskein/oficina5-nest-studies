f6ca892c09e1e6b3fb037d1a7c5a7fb7d6d44e57
Chipskein <abfn0905@gmail.com>
2025-03-11 17:46:57 -0300

    add:typeORM + db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
