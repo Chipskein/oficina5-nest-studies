b38194565f13394f1e8fe5947e5f464736751523
Chipskein <abfn0905@gmail.com>
2025-03-12 10:52:08 -0300

    add:gzip compression

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
-
+import * as compression from 'compression';
 
 async function bootstrap() {
   const appConfig={logger:new ConsoleLogger({
@@ -10,6 +10,7 @@ async function bootstrap() {
     logLevels:['log','error','warn','debug','verbose']
   })}
   const app = await NestFactory.create(AppModule,appConfig);
+  app.use(compression());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
